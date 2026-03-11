4dd034f2f1d889d5a47aa7775aa287e3fcbc7bd8
Rev <Musunuri.SaiRevanth@cognizant.com>
2026-03-11 11:26:02 +0530

    rrr

--- HelloWorld.cs
+++ HelloWorld.cs
@@ -6,5 +6,9 @@ public class HelloWorld
     {
         Console.WriteLine("Hey i am going well, World!");
         Console.WriteLine("Hey i am going well, World!");
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        Console.WriteLine("The sum of a and b is: " + sum);
     }
 }
